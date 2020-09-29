db73d41ee306878487391da1d4028018a61c3438
paul <dev@studiop.page>
2020-09-28 21:08:16 -0400

    Added poetry files

--- preprocess.py
+++ preprocess.py
@@ -1,1 +1,5 @@
-from note_seq import music_pb2, midi_file_to_note_sequence, midi_file_to_sequence_proto
+from note_seq import midi_file_to_note_sequence, midi_file_to_sequence_proto
+from note_seq.protobuf import music_pb2
+ns = midi_file_to_note_sequence('data/test4.mid')
+sequence = music_pb2.NoteSequence()
+print(ns)
---
+++ tests/test_midi_nn.py
@@ -0,0 +1,5 @@
+from midi_nn import __version__
+
+
+def test_version():
+    assert __version__ == '0.1.0'
