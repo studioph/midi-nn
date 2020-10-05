485f10f5b72a902958484c8cfb73232faa3d6af3
paul <dev@studiop.page>
2020-10-05 18:33:59 -0400

    Created new training script

--- utils.py
+++ utils.py
@@ -2,13 +2,13 @@ from note_seq.protobuf import music_pb2
 from note_seq.performance_encoder_decoder import NotePerformanceEventSequenceEncoderDecoder
 import numpy as np
 from sklearn.model_selection import train_test_split
-import tensorflow as tf
+# import tensorflow as tf
 
 # quit and print an error if there's no GPU
-def checkGPU():
-    if len(tf.config.list_physical_devices('GPU')) == 0:
-        raise EnvironmentError("No GPU detected!")
-        exit(code=1)
+# def checkGPU():
+#     if len(tf.config.list_physical_devices('GPU')) == 0:
+#         raise EnvironmentError("No GPU detected!")
+#         exit(code=1)
 
 """
 Converts a NoteSequence into a tensor of shape 
