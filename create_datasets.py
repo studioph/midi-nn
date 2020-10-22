afd980390d59a4dbb8a6392b899b4abf39a5c281
Paul Hutchings <dev@studiop.page>
2020-10-21 19:42:01 -0700

    Code formatting

--- create_datasets.py
+++ create_datasets.py
@@ -1,6 +1,7 @@
 import numpy as np
-import time, argparse, utils
+import time, argparse
 from sklearn.model_selection import train_test_split
+from midi_nn import utils
 
 # TODO
 # things to add to model input:
@@ -33,17 +34,23 @@ parser.add_argument('--test_size', dest="test_size", type=float, default=0.1, he
 args = parser.parse_args()
 
 start = time.time()
+
 print(f'Loading {args.input_file} file...')
 sequences = np.load(args.input_file, allow_pickle=True)
 print(f'{len(sequences)} sequences loaded')
+
 print(f'Creating samples of length {args.seq_length} notes...')
 seq_arrs = [utils.seq_to_arr(seq) for seq in sequences[:,1]]
 samples = create_subseqs(seq_arrs, args.seq_length)
+
 print(f'Creating train and test splits...')
 train, test = train_test_split(samples, test_size=args.test_size)
+
 print(f'Saving to {args.output_dir}/train.npy and {args.output_dir}/test.npy...')
 np.save(f'{args.output_dir}/train.npy', train)
 np.save(f'{args.output_dir}/test.npy', test)
+
 end = time.time() - start
+
 print('Done')
 print(f'Preprocessing took {round(end, 2)}s')
