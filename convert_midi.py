491f26c4da2d67fbdb7d462bf4611e195ef6e990
Paul Hutchings <dev@studiop.page>
2020-10-24 16:05:39 -0700

    Updated MIDI file conversion script to use numpy functions instead of in-file

--- convert_midi.py
+++ convert_midi.py
@@ -4,77 +4,47 @@ from multiprocessing import Pool
 import numpy as np
 
 """
-Converts a list of MIDI files into a dictionary map of filenames
-to bytes-serialized NoteSequences
+Converts a list of MIDI files into a list of NoteSequences
 
 Args:
-    args (string, list<string>): a tuple containing the input
-        directory path and a list of MIDI file names
+    args (string, iterable<string>): a tuple containing the input
+        directory path and an of MIDI file names
 
 Returns:
-    [filename, NoteSequence (bytes)]
+    list(NoteSequence) - A list NoteSequence objects
 """
 def convert_midi_files(args: tuple):
     input_dir, files = args
-    filemap = []
+    sequences = []
     for file in files:
         print(f'Converting {file}...')
         filename = file[:-4]
         input_path = input_dir + '/' + file
         sequence = midi_file_to_note_sequence(input_path)
-        filemap.append([filename, sequence])
-    return filemap
-
-"""
-Merges arrays
-
-Args:
-    [filename, NoteSequence]
-
-Returns:
-    A single level array with filename, NoteSequence pairs
-"""
-def merge(arrs: list):
-    merged = []
-    for arr in arrs:
-        merged += arr
-    return merged
-
-"""
-Splits an iterable (array, list) into n sublists
-
-Args:
-    arr (iterable): An iterable object
-    n (int): The number of pieces to split into
-
-Returns:
-    A list of length n containing equally sized sublists of arr
-"""
-def split(arr: list, n: int):
-    k, m = divmod(len(arr), n)
-    return (arr[i * k + min(i, m):(i + 1) * k + min(i + 1, m)] for i in range(n))
+        sequences.append(sequence)
+    return sequences
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
 
-    # --input_dir FOLDER --output_file FOLDER --threads 4
+    # --input_dir FOLDER --output_file FILE --processes 4
     parser.add_argument("--input_dir", dest = "input_dir", help = "Input folder containing MIDI files")
     parser.add_argument("--output_file", dest = "output_file", help = "Output file to write converted NoteSequences to")
-    parser.add_argument("--processes", dest="processes", type=int, default=4, help="Number of processes to use")
+    parser.add_argument("--processes", dest="processes", type=int, default=4, help="Number of processes to use in parallel")
 
     args = parser.parse_args()
     start = time.time()
 
     files = [file for file in os.listdir(args.input_dir) if file.lower().endswith('.mid')]
-    split_files = list(split(files, args.processes))
+    split_files = np.array_split(files, args.processes)
 
-    # parallelize the conversions. Merge the dictionaries at the end
+    # Parallelize the conversions. Merge the arrays at the end
     with Pool(args.processes) as pool:
-        results = pool.map(convert_midi_files, [(args.input_dir, split_files[i]) for i in range(len(split_files))])
-    filemaps = merge(results)
+        results = pool.map(convert_midi_files, [(args.input_dir, split_files[i]) for i in range(args.processes)])
+    sequences = np.flatten(results)
 
-    # Write dictionary to file for later use
-    np.save(args.output_file, filemaps)
+    # Write sequences to file for later use
+    np.save(args.output_file, sequences)
     end = time.time() - start
     print('Done')
     print(f'Conversion took {round(end / 60, 1)}m')
