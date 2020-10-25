42543fa47be05ff0ec0bff94d7e4dc707648ec76
Paul Hutchings <dev@studiop.page>
2020-10-24 17:00:26 -0700

    Moved functions into utils module

--- create_datasets.py
+++ create_datasets.py
@@ -11,21 +11,6 @@ from midi_nn import utils
 # velocity of next note?
 # articulation? Investigate Dorico files
 
-"""
-Creates subsequences of N notes from the data
-Args:
-    seqs: (list(NoteSequence)) - An array of NoteSequence objects
-    seq_length: (int) - The number of notes to include for each subsequence
-
-Returns:
-    numpy.ndarray - A Numpy array containing subsequences of length N
-"""
-def create_subseqs(seqs: list, seq_length: int):
-    subseqs = []
-    for seq in seqs:
-        subseqs += utils.batch_data(seq, seq_length)
-    return np.array(subseqs)
-
 parser = argparse.ArgumentParser()
 parser.add_argument("--input_file", dest = "input_file", type=str, help = "Input file containing NoteSequences")
 parser.add_argument("--output_dir", dest = "output_dir", type=str, help = "Output directory to write the train and test datasets to")
@@ -40,8 +25,8 @@ sequences = np.load(args.input_file, allow_pickle=True)
 print(f'{len(sequences)} sequences loaded')
 
 print(f'Creating samples of length {args.seq_length} notes...')
-seq_arrs = [utils.seq_to_arr(seq) for seq in sequences[:,1]]
-samples = create_subseqs(seq_arrs, args.seq_length)
+seq_arrs = [utils.seq_to_arr(seq) for seq in sequences]
+samples = utils.create_subseqs(seq_arrs, args.seq_length)
 
 print(f'Creating train and test splits...')
 train, test = train_test_split(samples, test_size=args.test_size)
--- midi_nn/utils.py
+++ midi_nn/utils.py
@@ -2,6 +2,7 @@ import numpy as np
 import torch
 import matplotlib.pyplot as plt
 from datetime import datetime
+from note_seq import midi_file_to_note_sequence
 
 """
 Ensures GPU is detected
@@ -13,6 +14,70 @@ def checkGPU():
     if not torch.cuda.is_available():
         raise EnvironmentError('GPU not detected!')
         exit(1)
+
+"""
+Converts a list of MIDI files into a list of NoteSequences
+
+Args:
+    args (string, iterable<string>): a tuple containing the input
+        directory path and an of MIDI file names
+
+Returns:
+    list(NoteSequence) - A list NoteSequence objects
+"""
+def convert_midi_files(args: tuple):
+    input_dir, files = args
+    sequences = []
+    for file in files:
+        print(f'Converting {file}...')
+        filename = file[:-4]
+        input_path = input_dir + '/' + file
+        sequence = midi_file_to_note_sequence(input_path)
+        sequences.append(sequence)
+    return sequences
+
+"""
+Extracts batches of length N from an array of samples
+Args:
+    arr (iterable) - An array-like object to be split into batches
+    batch_size (int) - The size of each batch
+Returns:
+"""
+def batch_data(arr: list, batch_size: int):
+    leftover = len(arr) % batch_size # discard excess samples
+    batches = np.split(np.array(arr[:-leftover]), int(len(arr) / batch_size))
+    return batches
+
+"""
+Creates subsequences of N notes from full NoteSequences
+Args:
+    seqs: (list(NoteSequence)) - An array of NoteSequence objects
+    seq_length: (int) - The number of notes to include for each subsequence
+
+Returns:
+    numpy.ndarray - A Numpy array containing subsequences of length N
+"""
+def create_subseqs(seqs: list, seq_length: int):
+    subseqs = []
+    for seq in seqs:
+        subseqs += batch_data(seq, seq_length)
+    return np.array(subseqs)
+
+"""
+Converts a NoteSequence into an array of note attributes
+Args:
+    seq (NoteSequence): The NoteSequence object to convert
+
+Returns:
+    (list(tuple)): A list of tuples with the following shape:
+        (velocity, pitch, duration)
+"""
+def seq_to_arr(seq):
+    notes = []
+    for note in seq.notes:
+        duration = round(note.end_time - note.start_time, 4)
+        notes.append([note.velocity, note.pitch, duration])
+    return notes
 
 """
 Encodes the real velocities as a one-hot tensor of length 128
@@ -32,22 +97,6 @@ def encode_dataset(ds):
     return [encode_velocities(arr) for arr in ds]
 
 """
-Extracts batches of length N from an array of samples
-Args:
-
-Returns:
-"""
-def batch_data(arr: list, batch_size: int):
-    batches = []
-    idxs = range(len(arr) + batch_size)[0::batch_size]
-    for idx in range(len(idxs[:-1])):
-        batches.append(arr[idxs[idx]:idxs[idx + 1]])
-    # discard incomplete batches
-    if len(batches[-1]) != batch_size:
-        batches.pop()
-    return batches
-
-"""
 Loads a saved Pytorch model to either the CPU or GPU (default)
 
 Returns:
@@ -56,23 +105,6 @@ Returns:
 def load_model(model_file_path: str, use_gpu=True):
     model = torch.load(model_file_path)
     return model.cuda()
-
-"""
-Converts a NoteSequence into an array of note attributes
-Args:
-    seq (NoteSequence): The NoteSequence object to convert
-
-Returns:
-    (list(tuple)): A list of tuples with the following shape:
-        (velocity, pitch, duration)
-"""
-def seq_to_arr(seq: NoteSequence):
-    notes = []
-    for note in seq.notes:
-        duration = round(note.end_time - note.start_time, 4)
-        notes.append([note.velocity, note.pitch, duration])
-    return notes
-
 
 """
 Plots the losses during training and validation as a line graph using Matplotlib
