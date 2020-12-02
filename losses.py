207f60dedbc1d8608220376412730cec514d6919
paul <dev@studiop.page>
2020-12-02 18:56:02 -0500

    Added time-seris loss plot

--- losses.py
+++ losses.py
@@ -1,6 +1,6 @@
 import numpy as np
 from numpy.lib.function_base import median
-import torch
+import torch, random
 import torch.nn as nn
 from torch.nn.modules import loss
 from midi_nn.basic import BasicVelocityPredictor
@@ -10,11 +10,11 @@ from midi_nn import utils
 test_file = 'data/test.npy'
 model_file = 'model_40.zip'
 loss_function = nn.MSELoss()
-predictor = BasicVelocityPredictor()
 
 model_losses = []
 basic_losses = []
 basic_avg_losses = []
+whole_avg_losses = []
 
 # using RMSE since it makes more logical sense in context of velocities
 def RMSELoss(yhat, y):
@@ -22,6 +22,14 @@ def RMSELoss(yhat, y):
 
 test = np.load(test_file)
 model = utils.load_model(model_file)
+
+# get avg velocity for the whole test dataset
+velocities = test[:,:,0]
+shape = np.shape(velocities)
+velocities = np.reshape(velocities, shape[0] * shape[1])
+avg = round(np.mean(velocities), 4)
+
+predictor = BasicVelocityPredictor(avg)
 
 # get losses for each validation sample
 print('acquiring losses from models...')
@@ -39,32 +47,56 @@ for sample in test:
     basic_loss = RMSELoss(basic_output, targets)
     basic_losses.append(basic_loss.item())
 
-    #get basic avg predictor losses
-    basic_avg_output = predictor.predict_avg(targets)
+    #get basic avg predictor losses for sample
+    basic_avg_output = predictor.predict_sample_avg(targets)
     basic_avg_loss = RMSELoss(basic_avg_output, targets)
     basic_avg_losses.append(basic_avg_loss.item())
 
-# plot losses as scatter plot
+    # get basic avg predictor losses for whole dataset
+    whole_avg_output = predictor.predict_whole_avg(targets)
+    whole_avg_loss = RMSELoss(whole_avg_output, targets)
+    whole_avg_losses.append(whole_avg_loss.item())
+
+# plot losses as histogram for each predictor
 print('plotting losses...')
-fig, ax = plt.subplots()
-x_range = range(len(model_losses))
-ax.scatter(x=['NN Model' for i in x_range], y=model_losses, color="g", marker=".", label="NN model loss")
-ax.scatter(x=['Basic Predictor' for i in x_range], y=basic_losses, color="r", marker=".", label='Basic predictor loss')
-ax.scatter(x=['Avg Predictor' for i in x_range], y=basic_avg_losses, color="b", marker=".", label="Avg predictor loss")
-ax.set_xlabel('Model')
-ax.set_ylabel('Loss')
-ax.legend()
-ax.grid(True)
-ax.set_title('Losses between various predictor models predicting velocity')
-plt.savefig('losses/loss_compare_scatter.png')
+fig, axs = plt.subplots(2, 2, sharey=True, tight_layout=True)
+fig.suptitle('Distribution of losses among model types')
+for pred, losses, x, y in zip(['NN Model', 'Basic Predictor', 'Sample Avg', 'Dataset Avg'], 
+                            [model_losses, basic_losses, basic_avg_losses, whole_avg_losses],
+                            [0,0,1,1], [0,1,0,1]):
+    axs[x][y].hist(losses)
+    axs[x][y].set_title(pred)
+    axs[x][y].set_xlabel('Loss')
 plt.show()
 
 #plot median loss of each model as bar chart
-median_losses = [np.median(model_losses), np.median(basic_losses), np.median(basic_avg_losses)]
+median_losses = [np.median(model_losses), np.median(basic_losses), np.median(basic_avg_losses), np.median(whole_avg_losses)]
 fig, ax = plt.subplots()
-plt.bar(x=['NN Model', 'Basic Predictor', 'Avg Predictor'], height=median_losses)
+plt.bar(x=['NN Model', 'Basic Predictor', 'Sample Avg', 'Dataset Avg'], height=median_losses)
 plt.xlabel('Model')
 plt.ylabel('Median loss')
 plt.title('Median loss of predictor models predicting velocity')
-plt.savefig('losses/loss_compare_bar.png')
+plt.show()
+
+# plot time series of losses for a single sample
+sample = random.choice(test)
+targets = torch.tensor(sample[:,0]).float()
+input = torch.tensor(sample[:,1:]).float().view(1, -1, 7)
+
+actual_vel = sample[:,0]
+model_vel = model(input.cuda()).view(-1).tolist()
+basic_vel = predictor.predict(targets).tolist()
+sample_avg = predictor.predict_sample_avg(targets).tolist()
+whole_avg = predictor.predict_whole_avg(targets).tolist()
+
+fig, ax = plt.subplots()
+plt.plot(actual_vel, color='royalblue', label='Actual')
+plt.plot(model_vel, color='green', label='NN Model')
+plt.plot(basic_vel, color='orange', label='Basic')
+plt.plot(sample_avg, color='indianred', label='Sample Avg')
+plt.plot(whole_avg, color='purple', label='Whole avg')
+plt.title('Time series comparison of model losses')
+plt.legend()
+plt.ylabel('Velocity')
+plt.xlabel('Note')
 plt.show()
--- midi_nn/basic.py
+++ midi_nn/basic.py
@@ -6,12 +6,16 @@ A basic predictor that simply uses the velocity of the previous note as the pred
 or the average velocity of the whole sequence for every value.
 """
 class BasicVelocityPredictor:
-    def __init__(self):
+    def __init__(self, avg):
+        self.avg = avg
         pass
 
     def predict(self, tensor):
         return torch.cat((tensor[-1:], tensor[:-1]))
 
-    def predict_avg(self, tensor):
+    def predict_sample_avg(self, tensor):
         avg_velocity = torch.mean(tensor).item()
         return torch.tensor(np.full((len(tensor)), avg_velocity))
+    
+    def predict_whole_avg(self, tensor):
+        return torch.tensor(np.full((len(tensor)), self.avg))
